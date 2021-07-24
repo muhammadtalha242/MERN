a9374fb8acdc01cbb9eedad27c10d4b529f60432
muhammadatalha242 <m.talha2244@gmail.com>
2021-07-24 20:56:12 +0500

    input validation

--- frontEnd/src/places/pages/NewPlace.js
+++ frontEnd/src/places/pages/NewPlace.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 import Input from "../../shared/components/FormElements/Input"
-
+import {VALIDATOR_REQUIRE} from '../../shared/util/validator'
 import "./NewPlace.css"
 
 function NewPlace() {
     return (
         <form className={`place-form`}>
-        <Input element="input" label="Title" type="text" errorText="Please Enter Valid Text"/>
+        <Input element="input" label="Title" type="text" errorText="Please Enter Valid Text" validators={[VALIDATOR_REQUIRE()]}/>
         </form>
     )
 }
--- frontEnd/src/shared/components/FormElements/Input.js
+++ frontEnd/src/shared/components/FormElements/Input.js
@@ -1,5 +1,7 @@
 import React, { useReducer } from 'react'
 
+import {validate} from "../../util/validator"
+
 import "./Input.css"
 
 
@@ -10,7 +12,12 @@ const inputReducer = (state, action) => {
             return {
                 ...state,            // initial state
                 value: action.val, //  value dispatched 
-                isValid: true
+                isValid: validate(action.val, action.validators)
+            }
+        case "TOUCHED":
+            return{
+                ...state,
+                isTouched:true
             }
         default:
             return state;   
@@ -19,33 +26,44 @@ const inputReducer = (state, action) => {
 
 const Input = (props) => {
 
-    const [inputState, dispatch] = useReducer(inputReducer, { value: "", isValid: false })
+    const [inputState, dispatch] = useReducer(inputReducer, { value: "", isTouched:false, isValid: false })
 
     const changeHandler = (event) => {
         dispatch({
             type: "CHANGE",
-            value: event.target.value
+            val: event.target.value,
+            validators: props.validators
         })
     }
 
-
+    const touchHandler=()=>{
+        dispatch({
+            type: "TOUCHED",
+        })  
+    }
     const element = props.element === "input" ? (
 
-        <input id={props.id} placeholder={props.placeholder} onChange={changeHandler} value={inputState.value} />
+        <input id={props.id} 
+        placeholder={props.placeholder} 
+        onChange={changeHandler} 
+        onBlur={touchHandler}
+        value={inputState.value} />
 
     ) : (
 
-        <textarea id={props.id} rows={props.rows || 3} onChange={changeHandler} value={inputState.value} />
+        <textarea id={props.id} rows={props.rows || 3} 
+        onBlur={touchHandler}
+        onChange={changeHandler} 
+        value={inputState.value} />
 
     );
     return (
-        <div className={`form-control ${!inputState.isValid && "form-control--invalid"}`}>
+        <div className={`form-control ${!inputState.isValid && inputState.isTouched  && "form-control--invalid"}`}>
             <label htmlFor={props.id}>{props.label}</label>
             {element}
-            {!inputState.isValid && <p>{props.errorText}</p>}
+            {!inputState.isValid && inputState.isTouched&&<p>{props.errorText}</p>}
         </div>
     )
 }
 
 export default Input
-
